bf7bb73079bfc87f77614e00521c9386527c1ade
nikita <nforvi@mail.ru>
2024-11-25 22:33:13 +0500

    jira_int module

--- addons/jira_integration/models/task_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, fields, models
-
-class Task(models.Model):
-    _name = 'jira.task'
-
-    task_id = fields.Integer(string='Task ID')
-    name = fields.Char(string='Project name')
-    description = fields.Text(string='Description')
